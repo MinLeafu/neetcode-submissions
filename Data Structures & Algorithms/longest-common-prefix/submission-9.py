d704ed6518d97ba4991930ff49891136e7c7e74c
Dream <dreamrae.yap@gmail.com>
2026-05-21 18:25:29 +0800

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            for j in range(len(prefix)):
+                if j == len(strs[i]) or prefix[j] != strs[i][j]:
+                    prefix = prefix[:j]
+                    break
+        return prefix
