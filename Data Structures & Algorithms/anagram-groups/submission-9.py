d869175c5740928714bd1040b38b759f6b075a4c
Dream <dreamrae.yap@gmail.com>
2026-05-22 14:46:22 +0800

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+
+        for str in strs:
+            chars = [0] * 26
+            for char in str:
+                chars[ord(char) - ord('a')] += 1
+            chars = tuple(chars)
+            hashmap[chars].append(str)
+
+        return list(hashmap.values())
