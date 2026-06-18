054e5cde65cb22d24e51b3fac0bcb88e32460601
Dream <dreamrae.yap@gmail.com>
2026-06-18 23:01:28 +0800

    Add: decode-ways - submission-14

---
+++ Data Structures & Algorithms/decode-ways/submission-14.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        left = 1
+        right = 1 if int(s[0]) != 0 else 0
+        for i in range(1, n):
+            temp = 0
+            if int(s[i]) != 0:
+                temp += right
+            if i > 0 and 10 <= int(s[i - 1:i + 1]) <= 26:
+                temp += left
+            left, right = right, temp
+        return right
